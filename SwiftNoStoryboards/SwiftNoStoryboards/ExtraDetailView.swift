ed7790e6389962b7c88854159533bc9126f95b45
Alan O'Connor <alan@getdrop.com>
2016-07-01 17:20:54 +0100

    Some messing around with views

---
+++ SwiftNoStoryboards/SwiftNoStoryboards/ExtraDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtraDetailView.swift
+//  SwiftNoStoryboards
+//
+//  Created by Alan O'Connor on 01/07/2016.
+//  Copyright © 2016 Alan O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ExtraDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.greenColor()
+    }
+    
+}
